dcdbae779b183d7599ae9df2a4b248d9583ef51f
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2023-12-07 09:17:03 +0100

    minor changes regarding the help text

--- src/main.cpp
+++ src/main.cpp
@@ -16,13 +16,16 @@ void argument_parsing_failed(const std::string &str) {
 
 void print_help() {
     const auto length {std::string("  --semantics [total|partial]").length()};
+    std::cout << std::endl;
     std::cout << "***** SwInE -- SMT with Integer Exponentiation *****" << std::endl;
+    std::cout << std::endl;
     std::cout << "usage: swine [args] input.smt2" << std::endl;
+    std::cout << std::endl;
     std::cout << "valid arguments:" << std::endl;
     std::cout << "  --solver [z3|cvc5]          : choose the backend SMT solver (default: z3)" << std::endl;
     std::cout << "  --semantics [total|partial] : choose the semantics for exp  (default: total)" << std::endl;
     for (const auto k: lemma_kind::values) {
-        const auto str {std::string("  --no-") + lemma_kind::str(k) + " lemmas"};
+        const auto str {std::string("  --no-") + lemma_kind::str(k)};
         const auto ws {length - str.length()};
         std::cout << str << std::string(ws, ' ') << " : disable " << k << " lemmas" << std::endl;
     }
@@ -39,9 +42,14 @@ void print_help() {
     std::cout << "  --version                   : print the SwInE version and exit" << std::endl;
     std::cout << "  --no-version                : omit the SwInE version at the end of the output" << std::endl;
     std::cout << "  --log                       : enable logging" << std::endl;
+    std::cout << std::endl;
 }
 
 int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        print_help();
+        return 0;
+    }
     int arg = 0;
     auto get_next = [&]() {
         if (arg < argc-1) {
@@ -53,87 +61,92 @@ int main(int argc, char *argv[]) {
     };
     Config config;
     std::optional<std::string> input;
+    SmtSolver solver;
     auto show_version {true};
-    while (++arg < argc) {
-        if (boost::iequals(argv[arg], "--solver")) {
-            const std::string str {get_next()};
-            if (boost::iequals(str, "z3")) {
-                config.solver_kind = SolverKind::Z3;
-            } else if (boost::iequals(str, "cvc5")) {
-                config.solver_kind = SolverKind::CVC5;
-            } else {
-                throw std::invalid_argument("unknown solver " + str);
-            }
-        } else if (boost::iequals(argv[arg], "--validate-sat")) {
-            config.validate_sat = true;
-        } else if (boost::iequals(argv[arg], "--validate-unsat")) {
-            const auto bound {std::stoi(get_next())};
-            if (bound >= 0) {
-                config.validate_unsat = bound;
-            }
-        } else if (boost::iequals(argv[arg], "--get-lemmas")) {
-            config.get_lemmas = true;
-        } else if (boost::iequals(argv[arg], "--log")) {
-            config.log = true;
-        } else if (boost::iequals(argv[arg], "--stats")) {
-            config.statistics = true;
-        } else if (boost::iequals(argv[arg], "--version")) {
-            version();
-            return 0;
-        } else if (boost::iequals(argv[arg], "--help")) {
-            print_help();
-            return 0;
-        } else if (boost::iequals(argv[arg], "--no-version")) {
-            show_version = false;
-        } else if (boost::iequals(argv[arg], "--semantics")) {
-            const std::string str {get_next()};
-            if (boost::iequals(str, "total")) {
-                config.semantics = Semantics::Total;
-            } else if (boost::iequals(str, "partial")) {
-                config.semantics = Semantics::Partial;
-            }  else {
-                throw std::invalid_argument("unknown semantics " + str);
-            }
-        } else if (boost::istarts_with(argv[arg], "--no-")) {
-            auto found {false};
-            for (const auto k: lemma_kind::values) {
-                if (boost::iequals(argv[arg], "--no-" + lemma_kind::str(k))) {
-                    config.deactivate(k);
-                    found = true;
-                    break;
+    try {
+        while (++arg < argc) {
+            if (boost::iequals(argv[arg], "--solver")) {
+                const std::string str {get_next()};
+                if (boost::iequals(str, "z3")) {
+                    config.solver_kind = SolverKind::Z3;
+                } else if (boost::iequals(str, "cvc5")) {
+                    config.solver_kind = SolverKind::CVC5;
+                } else {
+                    throw std::invalid_argument("unknown solver " + str);
                 }
-            }
-            if (!found) {
-                for (const auto k: preproc_kind::values) {
-                    if (boost::iequals(argv[arg], "--no-" + preproc_kind::str(k))) {
+            } else if (boost::iequals(argv[arg], "--validate-sat")) {
+                config.validate_sat = true;
+            } else if (boost::iequals(argv[arg], "--validate-unsat")) {
+                const auto bound {std::stoi(get_next())};
+                if (bound >= 0) {
+                    config.validate_unsat = bound;
+                }
+            } else if (boost::iequals(argv[arg], "--get-lemmas")) {
+                config.get_lemmas = true;
+            } else if (boost::iequals(argv[arg], "--log")) {
+                config.log = true;
+            } else if (boost::iequals(argv[arg], "--stats")) {
+                config.statistics = true;
+            } else if (boost::iequals(argv[arg], "--version")) {
+                version();
+                return 0;
+            } else if (boost::iequals(argv[arg], "--help")) {
+                print_help();
+                return 0;
+            } else if (boost::iequals(argv[arg], "--no-version")) {
+                show_version = false;
+            } else if (boost::iequals(argv[arg], "--semantics")) {
+                const std::string str {get_next()};
+                if (boost::iequals(str, "total")) {
+                    config.semantics = Semantics::Total;
+                } else if (boost::iequals(str, "partial")) {
+                    config.semantics = Semantics::Partial;
+                }  else {
+                    throw std::invalid_argument("unknown semantics " + str);
+                }
+            } else if (boost::istarts_with(argv[arg], "--no-")) {
+                auto found {false};
+                for (const auto k: lemma_kind::values) {
+                    if (boost::iequals(argv[arg], "--no-" + lemma_kind::str(k))) {
                         config.deactivate(k);
                         found = true;
                         break;
                     }
                 }
-            }
-            if (!found) {
+                if (!found) {
+                    for (const auto k: preproc_kind::values) {
+                        if (boost::iequals(argv[arg], "--no-" + preproc_kind::str(k))) {
+                            config.deactivate(k);
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+                if (!found) {
+                    argument_parsing_failed(argv[arg]);
+                }
+            } else if (!input) {
+                input = argv[arg];
+            } else {
                 argument_parsing_failed(argv[arg]);
             }
-        } else if (!input) {
-            input = argv[arg];
-        } else {
-            argument_parsing_failed(argv[arg]);
         }
-    }
-    SmtSolver solver;
-    switch (config.solver_kind) {
-    case SolverKind::Z3:
-        solver = smt::Z3SolverFactory::create(false);
-        break;
-    case SolverKind::CVC5:
-        solver = smt::Cvc5SolverFactory::create(false);
-        break;
-    default:
-        throw std::invalid_argument("unknown solver");
-    }
-    if (!input) {
-        throw std::invalid_argument("missing input file");
+        switch (config.solver_kind) {
+        case SolverKind::Z3:
+            solver = smt::Z3SolverFactory::create(false);
+            break;
+        case SolverKind::CVC5:
+            solver = smt::Cvc5SolverFactory::create(false);
+            break;
+        default:
+            throw std::invalid_argument("unknown solver");
+        }
+        if (!input) {
+            throw std::invalid_argument("missing input file");
+        }
+    } catch (const std::exception&) {
+        print_help();
+        throw;
     }
     SmtSolver swine {std::make_shared<Swine>(solver, config)};
     const auto res {SmtLibReader(swine).parse(*input)};
